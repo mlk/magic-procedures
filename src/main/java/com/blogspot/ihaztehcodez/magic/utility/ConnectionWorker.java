18f1fd08ff05ed7121467367b90d536a560d53e5
mlk <michael.lloyd.lee@gmail.com>
2012-03-05 21:18:10 +0000

    Documentation

--- src/main/java/com/blogspot/ihaztehcodez/magic/utility/ConnectionWorker.java
+++ src/main/java/com/blogspot/ihaztehcodez/magic/utility/ConnectionWorker.java
@@ -2,6 +2,7 @@ package com.blogspot.ihaztehcodez.magic.utility;
 
 import java.sql.SQLException;
 
+/** A connection worker executes a chunk of SQL. */
 public interface ConnectionWorker {
 	void execute(SQLWorker worker) throws SQLException;
 }
